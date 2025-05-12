0df32bf30ae49147d889b1c7bee8f30c7bbedef1
Arnav-S12 <arnavs.20124@gmail.com>
2025-05-12 22:25:41 +0530

    Add files via upload

---
+++ Demux_1x4/Demux_1x4.v
@@ -0,0 +1,12 @@
+//1x4 demux
+
+module Demux_1x4(
+	input I,s1,s2,
+	output a,b,c,d);
+
+ assign a = (s1) & (s2) & I;
+ assign b = (s1) &( ~s2) & I;
+ assign c = (~s1) & (s2) & I;
+ assign d = (~s1) & (~s2) & I;
+
+endmodule
